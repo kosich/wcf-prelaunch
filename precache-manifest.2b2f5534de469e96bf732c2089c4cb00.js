ad09c3a52ef4de6eb946a269f49ea724daa83e1b
Kostiantyn Palchyk <kosich@users.noreply.github.com>
2020-06-02 23:45:11 +0300

    update

---
+++ precache-manifest.2b2f5534de469e96bf732c2089c4cb00.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb40757d4e44f64180819b711f736f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f88deec12a985b3a4dd8",
+    "url": "/static/css/main.877ccae5.chunk.css"
+  },
+  {
+    "revision": "9550228aa363fa4a037c",
+    "url": "/static/js/2.b1170a60.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.b1170a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c594602bc66c213383",
+    "url": "/static/js/3.cb64925a.chunk.js"
+  },
+  {
+    "revision": "f88deec12a985b3a4dd8",
+    "url": "/static/js/main.fc06170c.chunk.js"
+  },
+  {
+    "revision": "df04c318a39efaa9ca0b",
+    "url": "/static/js/runtime-main.d52fb485.js"
+  },
+  {
+    "revision": "fe9df7b24c69c052db01b783dd47654c",
+    "url": "/static/media/contact.fe9df7b2.svg"
+  },
+  {
+    "revision": "4fb3597649fb54ad65a4ac77a87d709c",
+    "url": "/static/media/graph.4fb35976.svg"
+  }
+]);
